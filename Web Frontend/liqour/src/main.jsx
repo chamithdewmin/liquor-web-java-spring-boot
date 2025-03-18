d935cd0a097d5f65936438b044ff2efaa6a9c467
Your Name <youremail@example.com>
2025-03-18 07:41:33 +0530

    add files to the frontend

--- Web Frontend/liqour/src/main.jsx
+++ Web Frontend/liqour/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./index.js";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
